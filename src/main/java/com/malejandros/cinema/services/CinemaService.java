167de76fde0e4039412a8c40fa6daa72350a1194
Manuel Alejandro Salinas <isc.malejandro@gmail.com>
2023-03-29 13:24:57 -0600

    Add purchaseSeat method in CinemaService

--- src/main/java/com/malejandros/cinema/services/CinemaService.java
+++ src/main/java/com/malejandros/cinema/services/CinemaService.java
@@ -1,7 +1,12 @@
 package com.malejandros.cinema.services;
 
 import com.malejandros.cinema.models.Cinema;
+import com.malejandros.cinema.models.Seat;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Service
 public class CinemaService {
@@ -14,4 +19,24 @@ public class CinemaService {
     public Cinema getSeats() {
         return cinema;
     }
+
+    public Map<String, Object> purchaseSeat(Seat seat) {
+        try {
+            Seat s = cinema.getSeatByRowAndColumn(seat.getRow(), seat.getColumn());
+            if(s.isBooked()) {
+                throw new RuntimeException("The ticket has been already purchased!");
+            }
+            UUID token = UUID.randomUUID();
+            s.setToken(token);
+            s.setBooked(true);
+            Map<String, Object> map = new HashMap<>();
+            map.put("token", token);
+            map.put("ticket", s);
+            return map;
+        } catch (Exception e) {
+            throw new IndexOutOfBoundsException(e.getMessage());
+        }
+    }
+
+
 }
