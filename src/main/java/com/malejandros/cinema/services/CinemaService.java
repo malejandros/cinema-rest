db0c8486104ecac2aadcf6d709d4ddfac77a795e
Manuel Alejandro Salinas <isc.malejandro@gmail.com>
2023-03-29 12:14:39 -0600

    Add CinemaService

--- src/main/java/com/malejandros/cinema/services/CinemaService.java
+++ src/main/java/com/malejandros/cinema/services/CinemaService.java
@@ -1,2 +1,17 @@
-package com.malejandros.cinema.services;public class CinemaService {
+package com.malejandros.cinema.services;
+
+import com.malejandros.cinema.models.Cinema;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CinemaService {
+    private Cinema cinema;
+
+    public CinemaService() {
+        this.cinema = new Cinema(9, 9);
+    }
+
+    public Cinema getSeats() {
+        return cinema;
+    }
 }
