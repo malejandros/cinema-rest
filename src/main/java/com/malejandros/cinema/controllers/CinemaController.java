eea77872ce98ffc89eee967ed6fc997c8a6f47c9
Manuel Alejandro Salinas <isc.malejandro@gmail.com>
2023-03-29 13:23:17 -0600

    Add post method to purchase a seat

--- src/main/java/com/malejandros/cinema/controllers/CinemaController.java
+++ src/main/java/com/malejandros/cinema/controllers/CinemaController.java
@@ -1,13 +1,21 @@
 package com.malejandros.cinema.controllers;
 
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.malejandros.cinema.models.Cinema;
+import com.malejandros.cinema.models.Seat;
 import com.malejandros.cinema.services.CinemaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CinemaController {
@@ -25,6 +33,29 @@ public class CinemaController {
             String res = objectMapper.writeValueAsString(cinemaService.getSeats());
             return new ResponseEntity<>(res, HttpStatus.OK);
         } catch (Exception e) {
+            return new ResponseEntity<>("An error was occurred on the server, please try again!", HttpStatus.SERVICE_UNAVAILABLE);
+        }
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<String> purchaseSeat(@RequestBody Seat seat) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String res;
+        Map<String, Object> map = new HashMap<>();
+        try {
+            try {
+                map = cinemaService.purchaseSeat(seat);
+                res = objectMapper.writeValueAsString(map);
+                return new ResponseEntity<>(res, HttpStatus.OK);
+            } catch (JacksonException e) {
+                return new ResponseEntity<>("An error was occurred on the server, please try again!", HttpStatus.SERVICE_UNAVAILABLE);
+            } catch (Exception e) {
+
+                map.put("error", e.getMessage());
+                res = objectMapper.writeValueAsString(map);
+                return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+            }
+        } catch (JacksonException e) {
             return new ResponseEntity<>("An error was occurred on the server, please try again!", HttpStatus.SERVICE_UNAVAILABLE);
         }
     }
